be811b839ef9fba38c0716ac7fe8acda2d1ca26e
kayjeee <kagiso.killagram@gmail.com>
2023-09-15 22:15:44 +0200

    fixed linters

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,23 +1,22 @@
 class LikesController < ApplicationController
-    before_action :find_post, only: [:create, :destroy]
-  
-    def create
-      @like = @post.likes.new
-      @like.author = current_user
-      @like.save
-      redirect_to user_post_path(@post.user, @post)
-    end
-  
-    def destroy
-      @like = @post.likes.find_by(author: current_user)
-      @like&.destroy
-      redirect_to user_post_path(@post.user, @post)
-    end
-  
-    private
-  
-    def find_post
-      @post = Post.find(params[:post_id])
-    end
+  before_action :find_post, only: %i[create destroy]
+
+  def create
+    @like = @post.likes.new
+    @like.author = current_user
+    @like.save
+    redirect_to user_post_path(@post.user, @post)
   end
-  
+
+  def destroy
+    @like = @post.likes.find_by(author: current_user)
+    @like&.destroy
+    redirect_to user_post_path(@post.user, @post)
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:post_id])
+  end
+end
