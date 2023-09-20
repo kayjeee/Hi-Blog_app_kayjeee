13953a080876124c2a3c736da509109ac7edf770
kayjee <kagiso.killagram@gmail.com>
2023-09-20 10:37:46 +0200

    fixed post and user show

--- spec/views/user_show_spec.rb
+++ spec/views/user_show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature 'User Show', type: :feature do
-  let(:user) { User.create(name: 'Tom', photo: 'https://www.kasandbox.org/programming-images/avatars/leaf-blue.png', bio: 'He is a good programmar') }
+  let(:user) { User.create(name: 'Tom', photo: 'https://www.kasandbox.org/programming-images/avatars/leaf-blue.png', bio: 'He is a good programmer') }
   let!(:post1) { Post.create(author: user, title: 'first post', text: 'first text') }
   let!(:post2) { Post.create(author: user, title: 'second post', text: 'second text') }
   let!(:post3) { Post.create(author: user, title: 'third post', text: '3 text') }
@@ -20,16 +20,23 @@ RSpec.feature 'User Show', type: :feature do
     expect(page).to have_content('4 posts')
   end
 
-  scenario 'visiting the user show page, you see the 3 most recent post and bio of the user has written..' do
+  scenario 'visiting the user show page, you see the 3 most recent posts and bio of the user has written..' do
     visit user_path(user)
-    expect(page).to have_content('He is a good programmar')
+    expect(page).to have_content('He is a good programmer')
     expect(page).not_to have_content('fist text')
   end
 
-  scenario 'visiting the user show page, you see the 3 most recent post and bio of the user has written..' do
+  # New scenario to test if the user's first 3 posts are displayed
+  scenario 'visiting the user show page, you see the user\'s first 3 posts' do
     visit user_path(user)
-    expect(page).to have_content('He is a good programmar')
-    expect(page).not_to have_content('fist text')
+
+    # Check for the titles of the first 3 posts
+    expect(page).to have_content('first post')
+    expect(page).to have_content('second post')
+    expect(page).to have_content('third post')
+
+    # Ensure the 4th post is not displayed
+    expect(page).not_to have_content('4 post')
   end
 
   scenario 'has a link to the user index page' do
